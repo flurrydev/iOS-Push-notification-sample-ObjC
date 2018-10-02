4fe5ac98ca920202810fc825fcf171fa18209897
Yilun Xu <yilun.xu@oath.com>
2018-10-02 14:02:43 -0700

    two app delegate version

--- FlurryMarketingSampleApp_Obj/AppDelegate_Auto.m
+++ FlurryMarketingSampleApp_Obj/AppDelegate_Auto.m
@@ -1,31 +1,28 @@
 //
-//  AppDelegate.m
+//  AppDelegate_Auto.m
 //  FlurryMarketingSampleApp_Obj
 //
 //  Created by Yilun Xu on 10/2/18.
 //  Copyright © 2018 Flurry. All rights reserved.
 //
 
-#import "AppDelegate.h"
+#import "AppDelegate_Auto.h"
 #import "Flurry.h"
-#import <NotificationCenter/NotificationCenter.h>
-#import <UserNotifications/UserNotifications.h>
 #import "FlurryMessaging.h"
 #import "ViewController.h"
 #import "DeepLinkViewController.h"
 #import <CoreLocation/CoreLocation.h>
 
-@interface AppDelegate () {
+@interface AppDelegate_Auto () {
     CLLocationManager *locationManager;
 }
 
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate_Auto
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    NSLog(@"here manual delegate");
+    NSLog(@"auto delegate");
     
     // location service
     if ([CLLocationManager locationServicesEnabled]) {
@@ -36,32 +33,8 @@
         NSLog(@"Location Services are disabled");
         
     }
-    // MANUAL USE
-    // step 1 : register remote notification for ios version >= 10 or < 10
-    if (@available(iOS 10.0, *)) {
-        NSLog(@"version greater than or equal to 10");
-        if (@available(iOS 10.0, *)) {
-            UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-            center.delegate = self;
-            [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert) completionHandler:^(BOOL granted, NSError * _Nullable error){
-                if (!error && granted) {
-                    [application registerForRemoteNotifications];
-                    NSLog(@"Push registetration success!");
-                } else {
-                    NSLog(@"Push registration Failed. ERROR : %@ - %@", error.localizedFailureReason, error.localizedDescription);
-                }
-            }];
-        }
-    } else {
-        NSLog(@"version less than 10");
-        UIApplication *application = [UIApplication sharedApplication];
-        if ([application respondsToSelector:@selector(isRegisteredForRemoteNotifications)]) {
-            [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound | UIUserNotificationTypeAlert) categories:nil]];
-            [application registerForRemoteNotifications];
-        }
-            
-    }
-
+    [FlurryMessaging setAutoIntegrationForMessaging];
+    
     NSString *file = [[NSBundle mainBundle] pathForResource:@"FlurryMarketingConfig" ofType:@"plist"];
     NSDictionary *info = [NSDictionary dictionaryWithContentsOfFile:file];
     
@@ -137,53 +110,4 @@
     return YES;
 }
 
-# pragma mark - manual integration delegate method
-// set device token
--(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    [FlurryMessaging setDeviceToken:deviceToken];
-}
-
-// notification received & clicked (ios 7+)
--(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler {
-    // check if the notification is from Flurry
-    if ([FlurryMessaging isFlurryMsg:userInfo]) {
-        [FlurryMessaging receivedRemoteNotification:userInfo withCompletionHandler:^{
-            completionHandler(UIBackgroundFetchResultNewData);
-        }];
-    }
-}
-
-// notification received response (ios 10)
--(void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler  API_AVAILABLE(ios(10.0)){
-    if ([FlurryMessaging isFlurryMsg:response.notification.request.content.userInfo]) {
-        [FlurryMessaging receivedNotificationResponse:response withCompletionHandler:^{
-            completionHandler();
-        }];
-    }
-}
-
-
-// notification received in foreground (ios 10)
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler  API_AVAILABLE(ios(10.0)){
-    if ([FlurryMessaging isFlurryMsg:notification.request.content.userInfo]) {
-        [FlurryMessaging presentNotification:notification withCompletionHandler:^{
-            completionHandler(UNNotificationPresentationOptionAlert);
-        }];
-    }
-}
-
-
-# pragma mark - location service
-- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status{
-    if (status == kCLAuthorizationStatusAuthorizedWhenInUse || status == kCLAuthorizationStatusAuthorizedAlways) {
-        BOOL permission = [Flurry trackPreciseLocation:YES];
-        NSLog(@"%s: can track precise location: %d", __PRETTY_FUNCTION__, permission);
-    } else if (status == kCLAuthorizationStatusDenied) {
-        BOOL permission = [Flurry trackPreciseLocation:NO];
-        NSLog(@"%s: can track precise location: %d", __PRETTY_FUNCTION__, permission);
-    }
-}
-
-
 @end
-
--- FlurryMarketingSampleApp_Obj/main.m
+++ FlurryMarketingSampleApp_Obj/main.m
@@ -8,9 +8,23 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "AppDelegate_Auto.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        // default
+        // return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        
+        NSString* filePath = [[NSBundle mainBundle] pathForResource:@"FlurryNotificationConfig" ofType:@"json"];
+        NSData *data = [NSData dataWithContentsOfFile:filePath];
+        NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+        
+        BOOL manualStyle =  ![[[[jsonDict objectForKey:@"FlurryNotificationSettings"] objectForKey:@"APNS"] objectForKey:@"isAutoIntegration"] boolValue];
+        
+        NSString *delegateClass = manualStyle
+        ? NSStringFromClass([AppDelegate class])
+        : NSStringFromClass([AppDelegate_Auto class]);
+        return UIApplicationMain(argc, argv, nil, delegateClass);
+        
     }
 }
